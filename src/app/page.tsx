80f10134aa2de8c2d7ee5743fdea6b6c052d96d6
Mohd Sakib <mohdsakib@krapton.com>
2025-01-21 21:18:30 +0530

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@
 import HeroSection from "@/components/heroSection/page";
 import FeaturesSection from "@/components/featuresSection/page";
 import WorkingSection from "@/components/workingSection/page";
-import UseCaseSection from "@/components/useCaseSection/page";
-import DiscountSection from "@/components/discountSection/page";
-import PricingSection from "@/components/pricingSection/page";
-import FinalCtaSection from "@/components/finalCtaSection/page";
+// import UseCaseSection from "@/components/useCaseSection/page";
+// import DiscountSection from "@/components/discountSection/page";
+// import PricingSection from "@/components/pricingSection/page";
+// import FinalCtaSection from "@/components/finalCtaSection/page";
 import FooterSection from "@/components/footerSection/page";
 import NavSection from "@/components/navSection/page";
-import DownloadAppSection from "@/components/downloadAppSection/page";
+// import DownloadAppSection from "@/components/downloadAppSection/page";
 import AppSlider from "@/components/appSlider/page";
 
 export default function PademiForTeams() {
@@ -25,7 +25,7 @@ export default function PademiForTeams() {
 
       <AppSlider />
 
-      <UseCaseSection />
+      {/* <UseCaseSection />
 
       <DiscountSection />
 
@@ -33,7 +33,7 @@ export default function PademiForTeams() {
 
       <PricingSection />
 
-      <FinalCtaSection />
+      <FinalCtaSection /> */}
 
       <FooterSection />
     </div>
