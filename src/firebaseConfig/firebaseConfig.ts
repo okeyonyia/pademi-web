e62239c596b8bac0bcdf58876ccdf433e32ab72c
Sahilchaudharyy <120932502+Sahilchaudharyy@users.noreply.github.com>
2025-03-24 19:10:16 +0530

    fix firebase key to env secure

--- src/firebaseConfig/firebaseConfig.ts
+++ src/firebaseConfig/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { getAuth } from "@firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqu7vQi2Vt--MOF9LJM1K1U5tdGWpBjjs",
-  authDomain: "pademi-b6470.firebaseapp.com",
-  projectId: "pademi-b6470",
-  storageBucket: "pademi-b6470.firebasestorage.app",
-  messagingSenderId: "870978795494",
-  appId: "1:870978795494:web:e11cc924d3ea000790c827"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
